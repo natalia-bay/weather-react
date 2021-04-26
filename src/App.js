a3d40b0fe0ff01b9006e50247d5c7a57e1ca7590
natalia-bay <nat.bayduraeva@gmail.com>
2021-04-26 21:18:54 +0800

    Removed search component and moved it to weather

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Search from "./Search";
 import Footer from "./Footer";
 import Weather from "./Weather";
 import "./App.css";
@@ -7,12 +6,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Search />
-        <div class="container city-box">
-          <Weather />
-        </div>
-      </div>
+      <Weather />
       <Footer />
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -12,79 +12,101 @@ export default function Weather() {
   };
   return (
     <div className="Weather">
-      <h1>Cologne</h1>
-      <div>
-        <h5>
-          Local time: <span id="local-time">18:46</span>
-        </h5>
+      <div className="container">
+        <div className="container search-field">
+          <form className="input-group">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Enter a city"
+              autoComplete="off"
+              autoFocus=""
+            />
+            <button className="btn btn-outline-secondary" type="submit">
+              <i className="fas fa-search" aria-hidden="true"></i>
+            </button>
+            <button className="btn btn-outline-secondary" type="button">
+              <i className="fas fa-map-marker-alt" aria-hidden="true"></i>
+            </button>
+          </form>
+        </div>
       </div>
 
-      <div className="row align-items-center current-weather">
-        <div className="col-2 current-temp">
-          <span className="temp-value degrees-now">12</span>
-          <sup className="degree-element">°</sup>
-        </div>
-        <div className="col-1 units">
-          <ul className="temp-unit">
-            <li>
-              <a href="/" class="active temp-cel">
-                C
-              </a>
-            </li>
-            <li>
-              <a href="/" className="temp-fahr">
-                F
-              </a>
-            </li>
-          </ul>
+      <div class="container city-box">
+        <h1>Cologne</h1>
+        <div>
+          <h5>
+            Local time: <span id="local-time">18:46</span>
+          </h5>
         </div>
 
-        <div className="col-12 col-md-8 weather-block">
-          <ul class="weather-description">
-            <li className="description">{weatherData.description}</li>
-            <li>
-              <img
-                src={weatherData.image}
-                className="icon"
-                alt="weather icon"
-              />
-            </li>
-          </ul>
-          <div className="row details">
-            <div className="col-sm-5">
-              <ul className="weather-details">
-                <li>
-                  <span className="details-icon">
-                    <i className="fas fa-temperature-low"></i>
-                  </span>
-                  Feels like <span>{weatherData.feelsLike}</span>°C
-                </li>
-                <li>
-                  <span className="details-icon">
-                    <i className="fas fa-ruler-vertical"></i>
-                  </span>
-                  <span className="adjustHumidity">Humidity </span>
-                  <span>{weatherData.humidity}</span>%
-                </li>
-              </ul>
-            </div>
-            <div className="col-sm-6 details2col">
-              <ul className="weather-details2">
-                <li>
-                  <span className="details-icon">
-                    <i className="fas fa-umbrella"></i>
-                  </span>
-                  <span className="adjustPrecipitation">Chance of rain </span>
-                  <span>{weatherData.rain}</span>%
-                </li>
-                <li>
-                  <span className="details-icon">
-                    <i class="fas fa-wind"></i>
-                  </span>
-                  <span className="adjustWind">Wind </span>
-                  <span>{weatherData.windSpeed}</span> km/h
-                </li>
-              </ul>
+        <div className="row align-items-center current-weather">
+          <div className="col-2 current-temp">
+            <span className="temp-value degrees-now">12</span>
+            <sup className="degree-element">°</sup>
+          </div>
+          <div className="col-1 units">
+            <ul className="temp-unit">
+              <li>
+                <a href="/" class="active temp-cel">
+                  C
+                </a>
+              </li>
+              <li>
+                <a href="/" className="temp-fahr">
+                  F
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-8 weather-block">
+            <ul class="weather-description">
+              <li className="description">{weatherData.description}</li>
+              <li>
+                <img
+                  src={weatherData.image}
+                  className="icon"
+                  alt="weather icon"
+                />
+              </li>
+            </ul>
+            <div className="row details">
+              <div className="col-sm-5">
+                <ul className="weather-details">
+                  <li>
+                    <span className="details-icon">
+                      <i className="fas fa-temperature-low"></i>
+                    </span>
+                    Feels like <span>{weatherData.feelsLike}</span>°C
+                  </li>
+                  <li>
+                    <span className="details-icon">
+                      <i className="fas fa-ruler-vertical"></i>
+                    </span>
+                    <span className="adjustHumidity">Humidity </span>
+                    <span>{weatherData.humidity}</span>%
+                  </li>
+                </ul>
+              </div>
+              <div className="col-sm-6 details2col">
+                <ul className="weather-details2">
+                  <li>
+                    <span className="details-icon">
+                      <i className="fas fa-umbrella"></i>
+                    </span>
+                    <span className="adjustPrecipitation">Chance of rain </span>
+                    <span>{weatherData.rain}</span>%
+                  </li>
+                  <li>
+                    <span className="details-icon">
+                      <i class="fas fa-wind"></i>
+                    </span>
+                    <span className="adjustWind">Wind </span>
+                    <span>{weatherData.windSpeed}</span> km/h
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
